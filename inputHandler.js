0a4f86131329b6d5be334e6822c09cd9d30af2a0
Thandi Matrose <thandi.matrose32@gmail.com>
2024-05-08 07:35:28 +0200

    Adding site structure

--- inputHandler.js
+++ inputHandler.js
@@ -1,6 +1,12 @@
 
+function moveTo(destination) {
+    const element = document.getElementById(destination);
+    element.scrollIntoView();
+}
+
+
 function changeToForm(target) {
-    const forms = document.getElementsByTagName("iframe");
+    const forms = document.getElementsByClassName("form");
     for (let i = 0; i < forms.length; i++) {
         forms[i].classList.add("invisible");
     }
@@ -12,21 +18,8 @@ function changeToForm(target) {
 }
 
 
-function changeToMap() {
-    const forms = document.getElementsByTagName("iframe");
-    for (let i = 0; i < forms.length; i++) {
-        forms[i].classList.add("invisible");
-    }
-    const map = document.getElementById("map_embed");
-    map.classList.remove("invisible");
-}
-
 function changeToGVR() {
-    const forms = document.getElementsByTagName("iframe");
-    for (let i = 0; i < forms.length; i++) {
-        forms[i].classList.add("invisible");
-    }
-    
+  
     const gvr = document.getElementById("gvr22")
     gvr.classList.remove("invisible");
     
